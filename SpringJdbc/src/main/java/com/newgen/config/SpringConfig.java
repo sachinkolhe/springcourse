110f8598822acae774f541a332af3e88e797cbdd
sachinkolhe909 <sachukolhe@gmai.com>
2024-09-21 08:20:53 +0530

    named parameter jdbc template

--- SpringJdbc/src/main/java/com/newgen/config/SpringConfig.java
+++ SpringJdbc/src/main/java/com/newgen/config/SpringConfig.java
@@ -1,5 +1,10 @@
 package com.newgen.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -8,11 +13,23 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class SpringConfig {
+	
+	/*
+	 * Connec
+	 * Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/newgen1", "root", "sachin909");
+			
+			
+			// Step 2: creating a statement
+			Statement stmt = connection.createStatement();
+			
+			// Step 3: execute and get result set
+			ResultSet rs = stmt.executeQuery("select * from student");
+	 */
 
 	@Bean
 	public DriverManagerDataSource createDatasource() {
 		DriverManagerDataSource datasource = new DriverManagerDataSource();
-		datasource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//		datasource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		datasource.setUrl("jdbc:mysql://localhost:3306/newgen1");
 		datasource.setUsername("root");
 		datasource.setPassword("sachin909");
@@ -26,10 +43,9 @@ public class SpringConfig {
 		return jdbcTemplate;
 	}
 	
-//	@Bean
-//	public NamedParameterJdbcTemplate mySqlNamedParameterJdbcTemplate() {
-//		NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(createDatasource());
-//		
-//		return jdbcTemplate;
-//	}
+	@Bean
+	public NamedParameterJdbcTemplate mySqlNamedParameterJdbcTemplate() {
+		NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(createDatasource());
+		return jdbcTemplate;
+	}
 }
--- SpringJdbc/src/main/java/com/newgen/main/App.java
+++ SpringJdbc/src/main/java/com/newgen/main/App.java
@@ -1,14 +1,19 @@
 package com.newgen.main;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import com.newgen.config.SpringConfig;
+import com.newgen.model.Student;
 
 /**
  * Hello world!
@@ -22,28 +27,37 @@ public class App
     	ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
       
     	//get bean of jdbcTemplate
-    	JdbcTemplate jdbcTemplate = applicationContext.getBean(JdbcTemplate.class);
+//    	JdbcTemplate jdbcTemplate = applicationContext.getBean(JdbcTemplate.class);
     	
-    	int id = 3;
-    	String name = "Balkrishna";
-    	double marks = 12.2d;
+//    	int id = 3;
+//    	String name = "Balkrishna";
+//    	double marks = 43.3d;
     	
-    	
-    	String insertQuery = "insert into student values(?,?,?)";
-    	
-    	String updateQuery = "update student set marks = 40 where id = 1";
-    	
-    	String deleteQuery = "delete from student where id = 3";
+    	//this is example of insert query
+//    	String insertQuery = "insert into student values(?,?,?)";
+//    	int updatedRows = jdbcTemplate.update(insertQuery, id, name, marks);
+//    	System.out.println("Added Rows into database : " + updatedRows);
     	
     	
-    	int updatedRows = jdbcTemplate.update(insertQuery, id, name, marks);
+    	//this is example of update query
+//    	String updateQuery = "update student set marks = ? where id = ?";
+//    	int updatedRows = jdbcTemplate.update(updateQuery, marks, id);
+//    	System.out.println("Updated Rows into database : " + updatedRows);
     	
-    	System.out.println("Updated/Added Rows into database : " + updatedRows);
     	
     	
-//    	NamedParameterJdbcTemplate jdbcTemplate = applicationContext.getBean(NamedParameterJdbcTemplate.class);
-//    	
-//    	int id = 2;
+//    	String deleteQuery = "delete from student where id = ?";
+//    	int deletedRows = jdbcTemplate.update(deleteQuery, id);
+//    	System.out.println("deleted Rows into database : " + deletedRows);
+    	
+
+    	
+    	
+    	//below example of NamedParameterJdbcTemplate
+    	//creating bean of NamedParameterJdbcTemplate
+    	NamedParameterJdbcTemplate jdbcTemplate = applicationContext.getBean(NamedParameterJdbcTemplate.class);
+    	
+//    	int id = 3;
 //    	String name = "Jon";
 //    	double marks = 12.2d;
 //    	
@@ -51,26 +65,57 @@ public class App
 //    	map.put("id", id);
 //    	map.put("name", name);
 //    	map.put("marks", marks);
-//    	
+    	
 //    	String query = "insert into student values(:id,:name,:marks)";
 //    	int updatedRows = jdbcTemplate.update(query, map);
 //    	System.out.println(updatedRows);
     
+
+    	//example to fetch for all student
+//    	String selectQuery = "select * from student";
+    	
+//    	List<Student> studentList = jdbcTemplate.query(selectQuery, new RowMapper<Student>() {
+//
+//			public Student mapRow(ResultSet resultSet, int arg1) throws SQLException {
+//				Student st = new Student();
+//				st.setId(resultSet.getInt("id"));
+//				st.setName(resultSet.getString("name"));
+//				st.setMarks(resultSet.getDouble("marks"));
+//				return st;
+//			}
+//		
+//    	});
+    	
+//    	List<Student> studentList = jdbcTemplate.query(selectQuery, (resultSet, columnIndex) -> {
+//    		Student st = new Student();
+//			st.setId(resultSet.getInt("id"));
+//			st.setName(resultSet.getString("name"));
+//			st.setMarks(resultSet.getDouble("marks"));
+//			return st;
+//    	});
+//    	
+//    	System.out.println(studentList);
+    	
+    	
+    	String selectQuery = "select * from student where id = :id";
+    	Map<String, Object> paramMap = new HashMap();
+    	paramMap.put("id", 1);
+    	
+    	Student student = jdbcTemplate.queryForObject(selectQuery, paramMap, new RowMapper<Student>() {
+
+			@Override
+			public Student mapRow(ResultSet resultSet, int arg1) throws SQLException {
+	    		Student st = new Student();
+				st.setId(resultSet.getInt("id"));
+				st.setName(resultSet.getString("name"));
+				st.setMarks(resultSet.getDouble("marks"));
+				return st;
+			}
+		});
+    	
+    	System.out.println(student);
+    	
     }
     
-//    public Stude findUserById(int id) {
-//        String sql = "SELECT * FROM student WHERE id = :id";
-//        Map<String, Object> params = new HashMap<>();
-//        params.put("id", id);
-//        
-//        return namedParameterJdbcTemplate.queryForObject(sql, params, new RowMapper<User>() {
-//            @Override
-//            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-//                User user = new User();
-//                user.setId(rs.getInt("id"));
-//                user.setUsername(rs.getString("username"));
-//                return user;
-//            }
-//        });
-//    }
+
 }
